09d746558781a8c533c5864a9d6e2f8af0ac843e
nehhhhha6 <n0370525@gmail.com>
2025-03-03 00:26:32 +0530

    update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+        hostname: "pm-s3---images.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname: "/**",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default nextConfig;
